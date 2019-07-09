9a521cfa006a75cd88fa3ecad7d6b939e1085d89
pengyuwei94 <pengyuwei1994@gmail.com>
2019-07-09 11:58:28 +0100

    check first 3 conditions, last one missing

--- R/compare_pvalue.R
+++ R/compare_pvalue.R
@@ -1,18 +1,26 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+compare_pvalue <- function(model1, model2) {
 
-hello <- function() {
-  print("Hello, world!")
+  #Check that models are specified
+  if(missing(model1)) stop("model one must be specified")
+  if(missing(model2)) stop("model two must be specified")
+
+  #Check that models are 'evreg' objects
+  m1 <- deparse(substitute(model1))
+  m2 <- deparse(substitute(model2))
+  m  <- c(m1, m2)
+  n  <- length(m)
+  for(i in 1:n){
+    if(!inherits(get(m[i], envir = parent.frame()), "evreg"))
+    stop("Use only with 'evreg' objects")
+  }
+
+  #Check that model 2 has more parameters than model 1
+  if((!all(names(model2$coefficients) %in% names(model1$coefficients)))){
+      warning("models may not be nested")
+  }
+
+  #Check that the maximised log-likelihoods are the correct way round
+
+  p_value <- stats::pchisq(model1$loglik, model2$loglik)
+  return(p_value)
 }
