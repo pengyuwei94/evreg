f7e0f6c0faebe4aa7b3904719f484e1fe2227015
pengyuwei94 <pengyuwei1994@gmail.com>
2019-07-14 09:55:28 +0100

    Second draft with errors

--- R/add1_LRT.R
+++ R/add1_LRT.R
@@ -1,13 +1,14 @@
 ####add 1 covariate on mu based on Likelihood-ratio-test
 ####alpha equals 0.05 by default
 
-add1_LRT_mu <- function(fit, data, alpha = 0.05){
-  #1. Check if fit is missing
-  if(missing(fit)) stop("fit must be specified")
+add1_LRT_mu <- function(fit, alpha = 0.05){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
 
-  y       <- fit$call$y
-  y_index <- which(colnames(data) == y)
-  X       <- data[-y_index]
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
 
 
   m <- deparse(substitute(fit))
@@ -22,30 +23,29 @@ add1_LRT_mu <- function(fit, data, alpha = 0.05){
     if(length(attr(fit$data$D$mu, "assign")) != 1){
 
       x_name <- all.vars(fit$formulae$mu)
-      index  <- which(colnames(data) == x_name) #This code gives ERROR since class(x_name) =call
-      #a data frame with covariates that are not in the mu formula
-      X <- X[-index]
+      index  <- which(colnames(data) == x_name)
+      #This code gives ERROR since class(x_name) =call
+      #Even using eval(x_name), index outputs 'interger(0)'
+
+      X <- X[-index]   #a data frame with covariates that are not in the mu formula
 
     }
-    #get names from rest of covariates
-    name <- names(X)
-    mu <- fit$formulae$mu
-    sigma <- fit$formulae$sigma
-    xi <- fit$formulae$xi
+    name <- names(X)   #get names from rest of covariates
 
-    #likelihood-ratio-test
+    ##likelihood-ratio-test
     p_table <- c()
+    m_list  <- list()
+
     for(i in 1:length(name)){
-      add1 <- gevreg(y, data, mu = as.formula(paste(mu, paste(name[i], collapse = "+"))), sigma = sigma, xi = xi)
-      #store all the p-values in one vector
-      p_table[i] <- compare_pvalue(fit, add1)
+      mu          <- update(fit$formulae$mu, ~. + name[i]) #update mu formula
+      m_list[[i]] <- update(fit, mu = mu)                  #update a model call by adding one additional covariate on mu
+      p_table[i]  <- compare_pvalue(fit, add1)         #store all the p-values in one vector
     }
 
-    #check significance
+    ##check significance
     if(min(p_table) < alpha){
-      x_i <- which(p_table == min(p_table))
-      add1 <- gevreg(y, data, mu = as.formula(paste(mu, paste(name[xi], collapse = "+"))), sigma = sigma, xi = xi)
-      return(add1)
+      x_i  <- which(p_table == min(p_table))
+      return(m_list[[1]])
     }else{
       cat("None of the likelihood-ratio-tests are significant")
       return(fit)
