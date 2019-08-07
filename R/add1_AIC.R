7d358474be8bc13f49b647df9358a4143030d0d9
pengyuwei94 <pengyuwei1994@gmail.com>
2019-08-07 23:55:11 +0100

    complete AIC version, with bugs on forward_AIC_xi

--- R/add1_AIC.R
+++ R/add1_AIC.R
@@ -1,38 +1,51 @@
-
 #' Add one possible covariate on GEV parameter based on AIC
 #'
 #' Add a single term to either mu, sigma, and xi based on AIC.
 #'
-#' add1_AIC_mu(fit)
-#' add1_AIC_sigma(fit)
-#' add1_AIC_xi(fit)
-#'
-#' @param fit A model of class "gevreg".
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if a covariate has been added or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{added_covariate}{A character vector shows added covariate}
 #'     \item{AIC}{AIC values for both input model and output model if two models
 #'     are different.}
 #' @examples
 #'
 #' ### Fremantle sea levels
 #' # Parameter mu
-#' f0 <- gevreg(SeaLevel, data = evreg::fremantle)
+#' f0 <- gevreg(SeaLevel, data = evreg::fremantle[,-1])
 #' add1_AIC_mu(f0)
 #'
 #' # Parameter sigma
-#' f3 <- gevreg(y = SeaLevel, data = evreg::fremantle[-1], mu = ~Year01 + SOI)
+#' f3 <- gevreg(y = SeaLevel, data = evreg::fremantle[,-1], mu = ~Year01 + SOI)
 #' add1_AIC_sigma(f3)
 #'
 #' # Parameter xi
-#' f4 <- gevreg(y = SeaLevel, data = evreg::fremantle[-1], mu = ~Year01 + SOI, sigma = ~SOI)
+#' f4 <- gevreg(y = SeaLevel, data = evreg::fremantle[,-1], mu = ~Year01 + SOI, sigma = ~SOI)
 #' add1_AIC_xi(f4)
 #'
+#'
+#' ### Annual Maximum and Minimum Temperature
+#'
+#' P0 <- gevreg(y = TMX1, data = PORTw[, -1])
+#' add1_AIC_mu(P0)
+#' add1_AIC_sigma(P0)
+#' add1_AIC_xi(P0)
+#'
+#' @name add1_AIC
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname add1_AIC
 #' @export
 add1_AIC_mu <- function(fit){
   ##1. Check if input arguments are missing
@@ -51,7 +64,7 @@ add1_AIC_mu <- function(fit){
   n       <- ncol(X) + 1          #number of covatiates +1 in the data
 
   ##3. Check if input fit already has all covariates on mu
-  n_mu <- length(attr(fit$data$D$mu, "assign"))
+  n_mu <- ncol(fit$data$D$mu)
   if(n_mu == n){
     stop("All covariates has been added on mu for input fit")
   }
@@ -65,12 +78,9 @@ add1_AIC_mu <- function(fit){
 
     ###Extract covariates on mu if there are in input fit
     if(n_mu != 1){
-
       x_name <- all.vars(fit$formulae$mu)
       index  <- which(colnames(X) %in% x_name)
-
       X <- X[-index]   #a data frame with covariates that are not in the mu formula
-
     }
     name <- names(X)   #get names from rest of covariates
 
@@ -80,8 +90,21 @@ add1_AIC_mu <- function(fit){
     aic <- c()
     m_list <- list()
     for(i in 1:length(name)){
+      # adding more variables on mu, one at a time
       mu  <- update(fit$formulae$mu, paste("", name[i], sep = "~.+"))  #update mu formula
-      new_fit     <- update(fit, mu = mu)     #update fit
+      # update a model call by adding one additional covariate on mu
+      # when we fit a new model in which an extra covariate is added,
+      # we use starting values based on the fit of the smaller model.
+      # The start value for the new added variable will be set to be zero.
+      fcoefs <- fit$coefficients
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- c(unname(fcoefs[1:n_mu]), 0)
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      xistart <- unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)])
+      new_fit     <- update(fit, mu = mu,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart =xistart)     #update fit
       m_list[[i]] <- new_fit
       aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
     }
@@ -93,7 +116,9 @@ add1_AIC_mu <- function(fit){
     #    return this model.  Otherwise, return the original model
 
     if(min(aic) < AIC(fit)){
-      output <- list()
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
       output$Input_fit <- fit$call
 
       list <- list()
@@ -103,24 +128,22 @@ add1_AIC_mu <- function(fit){
 
       output$AIC        <- c(AIC(fit), min(aic))
       names(output$AIC) <- c("Input model", "Output model")
-
-      return(output)
     }else{
-      output <- list()
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
-
   }
-  #else for pp fit
+  return(output)
 
 }
 
 
+# ----------------------------- sigma ---------------------------------
 
-
+#' @rdname add1_LRT
+#' @export
 add1_AIC_sigma <- function(fit){
   ##1. Check if input arguments are missing
   if(missing(fit))  stop("fit must be specified")
@@ -138,8 +161,8 @@ add1_AIC_sigma <- function(fit){
   n       <- ncol(X) + 1          #number of covatiates +1 in the data
 
   ##3. Check if input fit already has all covariates on sigma
-  n_sig <- length(attr(fit$data$D$sigma, "assign"))
-  if(n_sig == n){
+  n_sigma <- ncol(fit$data$D$sigma)
+  if(n_sigma == n){
     stop("All covariates has been added on sigma for input fit")
   }
 
@@ -151,25 +174,34 @@ add1_AIC_sigma <- function(fit){
   if(inherits(get(m, envir = parent.frame()), "gev")){
 
     ###Extract covariates on sigma if there are in input fit
-    if(n_sig != 1){
-
+    if(n_sigma != 1){
       x_name <- all.vars(fit$formulae$sigma)
       index  <- which(colnames(X) %in% x_name)
-
       X <- X[-index]   #a data frame with covariates that are not in the sigma formula
-
     }
     name <- names(X)   #get names from rest of covariates
 
     #General steps
-    # 1. Fit all of the possible models obtained by adding 1 covariate on sigma
-    #    to the original model
+    # 1. Fit all of the possible models obtained by adding 1 covariate to the original model
     # 2. Calculate the value of AIC for all these models.
     aic <- c()
     m_list <- list()
     for(i in 1:length(name)){
+      # adding more variables on sigma, one at a time
       sigma  <- update(fit$formulae$sigma, paste("", name[i], sep = "~.+"))  #update sigma formula
-      new_fit     <- update(fit, sigma = sigma)     #update fit
+      # update a model call by adding one additional covariate on sigma
+      # when we fit a new model in which an extra covariate is added,
+      # we use starting values based on the fit of the smaller model.
+      # The start value for the new added variable will be set to be zero.
+      fcoefs <- fit$coefficients
+      n_mu   <- ncol(fit$data$D$mu)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- c(unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)]), 0)
+      xistart <- unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)])
+      new_fit     <- update(fit, sigma = sigma,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart =xistart)     #update fit
       m_list[[i]] <- new_fit
       aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
     }
@@ -181,34 +213,34 @@ add1_AIC_sigma <- function(fit){
     #    return this model.  Otherwise, return the original model
 
     if(min(aic) < AIC(fit)){
-      output <- list()
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
       output$Input_fit <- fit$call
 
       list <- list()
-      list$sigma  <- m_list[[x_i]]$formulae$sigma
-      list$fit    <- m_list[[x_i]]$call
+      list$sigma <- m_list[[x_i]]$formulae$sigma
+      list$fit   <- m_list[[x_i]]$call
       output$Output_fit <- list
 
       output$AIC        <- c(AIC(fit), min(aic))
       names(output$AIC) <- c("Input model", "Output model")
-
-      return(output)
     }else{
-      output <- list()
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
-
   }
-  #else for pp fit
+  return(output)
 
 }
 
 
+# ----------------------------- xi ---------------------------------
 
-
+#' @rdname add1_LRT
+#' @export
 add1_AIC_xi <- function(fit){
   ##1. Check if input arguments are missing
   if(missing(fit))  stop("fit must be specified")
@@ -226,7 +258,7 @@ add1_AIC_xi <- function(fit){
   n       <- ncol(X) + 1          #number of covatiates +1 in the data
 
   ##3. Check if input fit already has all covariates on xi
-  n_xi <- length(attr(fit$data$D$xi, "assign"))
+  n_xi <- ncol(fit$data$D$xi)
   if(n_xi == n){
     stop("All covariates has been added on xi for input fit")
   }
@@ -238,25 +270,36 @@ add1_AIC_xi <- function(fit){
   ###################################################################
   if(inherits(get(m, envir = parent.frame()), "gev")){
 
-    ###Extract covariates on mu if there are in input fit
+    ###Extract covariates on xi if there are in input fit
     if(n_xi != 1){
-
       x_name <- all.vars(fit$formulae$xi)
       index  <- which(colnames(X) %in% x_name)
-
       X <- X[-index]   #a data frame with covariates that are not in the xi formula
-
     }
     name <- names(X)   #get names from rest of covariates
 
     #General steps
-    # 1. Fit all of the possible models obtained by adding 1 covariate on xi to the original model
+    # 1. Fit all of the possible models obtained by adding 1 covariate to the original model
     # 2. Calculate the value of AIC for all these models.
     aic <- c()
     m_list <- list()
     for(i in 1:length(name)){
+      # adding more variables on xi, one at a time
       xi  <- update(fit$formulae$xi, paste("", name[i], sep = "~.+"))  #update xi formula
-      new_fit     <- try(update(fit, xi = xi))     #update fit
+      # update a model call by adding one additional covariate on xi
+      # when we fit a new model in which an extra covariate is added,
+      # we use starting values based on the fit of the smaller model.
+      # The start value for the new added variable will be set to be zero.
+      fcoefs  <- fit$coefficients
+      n_mu    <- ncol(fit$data$D$mu)
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      xistart <- c(unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)]), 0)
+      new_fit <- try(update(fit, xi = xi,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart =xistart))     #update fit
       m_list[[i]] <- new_fit
       aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
     }
@@ -268,7 +311,9 @@ add1_AIC_xi <- function(fit){
     #    return this model.  Otherwise, return the original model
 
     if(min(aic) < AIC(fit)){
-      output <- list()
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
       output$Input_fit <- fit$call
 
       list <- list()
@@ -278,18 +323,14 @@ add1_AIC_xi <- function(fit){
 
       output$AIC        <- c(AIC(fit), min(aic))
       names(output$AIC) <- c("Input model", "Output model")
-
-      return(output)
     }else{
-      output <- list()
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
-
   }
-  #else for pp fit
+  return(output)
 
 }
 
--- R/forward_AIC.R
+++ R/forward_AIC.R
@@ -1,91 +1,211 @@
-
 #' AIC-based Forward Selection on GEV Parameter
 #'
 #' Significance controlled variable selection selects variables in either
 #' mu, sigma, and xi with forward direction based on AIC.
 #'
-#' forward_AIC_mu(fit)
-#' forward_AIC_sigma(fit)
-#' forward_AIC_xi(fit)
 #'
-#' @param fit A model of class "gevreg".
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if a covariate has been added or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{added_covariate}{A character vector shows added covariates}
 #'     \item{AIC}{AIC values for both input model and output model if two models
 #'     are different.}
 #' @examples
 #'
 #' ### Fremantle sea levels
 #'
-#' f0 <- gevreg(SeaLevel, data = evreg::fremantle)
+#' f0 <- gevreg(SeaLevel, data = evreg::fremantle[,-1])
 #' forward_AIC_mu(f0)
 #'
 #'
 #' ### Annual Maximum and Minimum Temperature
 #'
-#' library(extRemes)
-#' data(PORTw)
-#' PORTw$Year <- (PORTw$Year - min(PORTw$Year)) / (max(PORTw$Year) - min(PORTw$Year))
-#' P0 <- gevreg(TMX1, data = PORTw)
-#'
+#' P0 <- gevreg(y = TMX1, data = PORTw[, -1])
+#' forward_AIC_mu(P0)
+#' forward_AIC_sigma(P0)
+#' @name forward_LRT
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname forward_LRT
 #' @export
 forward_AIC_mu <- function(fit){
-
   # Number of covariates in the data
   cov_n <- ncol(eval(fit$call$data)) - 1
   new_fit <- add1_AIC_mu(fit)
-
-  # The length of return list from new_fit
-  # If this is equal to 3 then we have added a covariate
-  # If this is equal to 2 then we haven't
-  re_n    <- as.numeric(length(new_fit))
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
 
   #Check if the above new_fit is full model
-  if(re_n != 2){
-    cov_new <- length(all.vars(new_fit$Output_fit$mu))
+  if(new_fit$Note == "covariate added"){
+    cov_new <- length(all.vars(new_fit$formulae$mu))
     # If we have added all the covariates then we stop
     # Otherwise, we try adding more variables, one at a time, using
-    # add1_LRT_mu().  We stop when either
-    # 1. add1_LRT_mu() doesn't add a covariate (re_n = 2), or
+    # add1_AIC_mu().  We stop when either
+    # 1. add1_AIC_mu() doesn't add a covariate (new_fit$Note != "covariate added"), or
     # 2. we have added all the covariates (cov_new = cov_n)
-    # Therefore, we continue to loop while re_n != 2 and cov_new < cov_n
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
     if(cov_new == cov_n){
-      return(new_fit)
+      newer_fit <- new_fit
     }else{
-      while (re_n != 2 & cov_new < cov_n) {
-        mu      <- new_fit$Output_fit$mu
-        model   <- eval(new_fit$Output_fit$fit)
-        new_fit <- add1_AIC_mu(model)
-        re_n    <- as.numeric(length(new_fit))   #length of return list from new_fit
-        cov_new <- length(all.vars(new_fit$Output_fit$mu))
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_AIC_mu(new_fit)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(new_fit$Output_fit$mu))
+        new_fit   <- newer_fit
       }
-      if(re_n == 2){
-        ##Make better output
-        # AIC
-        new_fit$AIC <- c(AIC(fit), AIC(model))
+      # Make better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
         names(new_fit$AIC) <- c("Input model", "Output model")
         # Output_fit
         list <- list()
-        list$mu  <- mu
-        list$fit <- new_fit$Input_fit
-        new_fit$Note <- list
+        list$mu  <- newer_fit$formulae$mu
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
         # Input_fit
         new_fit$Input_fit <- fit$call
 
-
-        # Rename output list
-        names(new_fit) <- c("Input_fit", "Output_fit", "AIC")
       }
 
 
     }
+  }else {
+    newer_fit <- new_fit
   }
 
   return(new_fit)
 }
+
+
+# ----------------------------- sigma ---------------------------------
+
+#' @rdname forward_LRT
+#' @export
+forward_AIC_sigma <- function(fit){
+  # Number of covariates in the data
+  cov_n <- ncol(eval(fit$call$data)) - 1
+  new_fit <- add1_AIC_sigma(fit)
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
+
+  #Check if the above new_fit is full model
+  if(new_fit$Note == "covariate added"){
+    cov_new <- length(all.vars(new_fit$formulae$sigma))
+    # If we have added all the covariates then we stop
+    # Otherwise, we try adding more variables, one at a time, using
+    # add1_AIC_sigma().  We stop when either
+    # 1. add1_AIC_sigma() doesn't add a covariate (new_fit$Note != "covariate added"), or
+    # 2. we have added all the covariates (cov_new = cov_n)
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
+    if(cov_new == cov_n){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_AIC_sigma(new_fit)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(new_fit$Output_fit$sigma))
+        new_fit   <- newer_fit
+      }
+      # Make better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
+        names(new_fit$AIC) <- c("Input model", "Output model")
+        # Output_fit
+        list <- list()
+        list$sigma <- newer_fit$formulae$sigma
+        list$fit   <- newer_fit$call
+        newer_fit$Output_fit <- list
+        # Input_fit
+        new_fit$Input_fit <- fit$call
+
+      }
+
+
+    }
+  }else {
+    newer_fit <- new_fit
+  }
+
+  return(new_fit)
+}
+
+
+# ----------------------------- xi ---------------------------------
+
+#' @rdname forward_LRT
+#' @export
+forward_AIC_xi <- function(fit){
+  # Number of covariates in the data
+  cov_n <- ncol(eval(fit$call$data)) - 1
+  new_fit <- add1_AIC_xi(fit)
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
+
+  #Check if the above new_fit is full model
+  if(new_fit$Note == "covariate added"){
+    cov_new <- length(all.vars(new_fit$formulae$xi))
+    # If we have added all the covariates then we stop
+    # Otherwise, we try adding more variables, one at a time, using
+    # add1_AIC_xi().  We stop when either
+    # 1. add1_AIC_xi() doesn't add a covariate (new_fit$Note != "covariate added"), or
+    # 2. we have added all the covariates (cov_new = cov_n)
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
+    if(cov_new == cov_n){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_AIC_xi(new_fit)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(new_fit$Output_fit$xi))
+        new_fit   <- newer_fit
+      }
+      # Make better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
+        names(new_fit$AIC) <- c("Input model", "Output model")
+        # Output_fit
+        list <- list()
+        list$xi  <- newer_fit$formulae$xi
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
+        # Input_fit
+        new_fit$Input_fit <- fit$call
+
+      }
+
+
+    }
+  }else {
+    newer_fit <- new_fit
+  }
+
+  return(new_fit)
+}
+
+
+
+
+
