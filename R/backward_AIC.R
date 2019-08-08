95bffe066d3169545159d61b177fb73a389fcdb8
pengyuwei94 <pengyuwei1994@gmail.com>
2019-08-08 18:49:22 +0100

    complete version for drop/backward AIC with some bugs

--- R/backward_AIC.R
+++ R/backward_AIC.R
@@ -3,19 +3,18 @@
 #' Significance controlled variable selection selects variables in either
 #' mu, sigma, and xi with backward direction based on AIC.
 #'
-#' backward_AIC_mu(fit)
-#' backward_AIC_sigma(fit)
-#' backward_AIC_xi(fit)
-#'
-#' @param fit A model of class "gevreg".
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if covariates have been dropped or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{dropped_covariate}{A character vector shows dropped covariates}
 #'     \item{AIC}{AIC values for both input model and output model if two models
 #'     are different.}
 #' @examples
@@ -30,63 +29,170 @@
 #'
 #' ### Annual Maximum and Minimum Temperature
 #'
-#' library(extRemes)
-#' data(PORTw)
-#' PORTw$Year <- (PORTw$Year - min(PORTw$Year)) / (max(PORTw$Year) - min(PORTw$Year))
-#' P6 <- gevreg(TMX1, data = PORTw, mu = ~MTMAX + AOindex + Year + STDTMAX + STDMIN + MDTR)
+#' P6 <- gevreg(TMX1, data = PORTw[,-1], mu = ~MTMAX + AOindex + STDTMAX + STDMIN + MDTR)
+#' P7 <- gevreg(TMX1, data = PORTw[,-1], sigma = ~MTMAX + STDTMAX + STDMIN + MDTR)
+#' P8 <- gevreg(TMX1, data = PORTw[,-1], xi = ~MTMAX + STDTMAX + STDMIN + MDTR)
 #' backward_AIC_mu(P6)
+#' backward_AIC_sigma(P7)
+#' backward_AIC_xi(P8)
 #'
+#' @name backward_AIC
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname backward_AIC
 #' @export
 backward_AIC_mu <- function(fit){
 
   new_fit <- drop1_AIC_mu(fit)
-  # The length of return list from new_fit
-  # If this is equal to 3 then we have added a covariate
-  # If this is equal to 2 then we haven't
-  re_n    <- as.numeric(length(new_fit))
-
+  # Store dropped_covariate for later use
+  dropped <- new_fit$dropped_covariate
   #Check if the above new_fit is null model
-  if(re_n != 2){
-    var <- as.character(new_fit$Output_fit$mu)[2]
-
+  if(new_fit$Note == "covariate dropped"){
+    cov_new <- ncol(new_fit$data$D$mu)
     # If we have dropped all the covariates then we stop
     # Otherwise, we try to dropp more variables, one at a time, using
     # drop1_AIC_mu().  We stop when either
-    # 1. drop1_AIC_mu() doesn't drop a covariate (re_n = 2), or
-    # 2. we have dropped all the covariates (var == "1")
-    # Therefore, we continue to loop while re_n != 2 and var != "1"
-    if(var == "1"){
-      return(new_fit)
+    # 1. drop1_AIC_mu() doesn't drop a covariate (new_fit$Note != "covariate dropped"), or
+    # 2. we have dropped all the covariates (cov_new == 1)
+    # Therefore, we continue to loop while new_fit$Note == "covariate dropped" and cov_new != 1
+    if(cov_new == 1){
+      newer_fit <- new_fit
     }else{
-      while (re_n != 2 & var != "1") {
-        mu      <- new_fit$Output_fit$mu
-        model   <- eval(new_fit$Output_fit$fit)
-        new_fit <- drop1_AIC_mu(model)
-
-        re_n    <- as.numeric(length(new_fit))   #length of return list from new_fit
-        var     <- as.character(new_fit$Output_fit$mu)[2]
+      newer_fit <- new_fit
+      while (new_fit$Note == "covariate dropped" & cov_new != 1) {
+        newer_fit <- drop1_AIC_mu(new_fit)
+        dropped <- append(dropped, newer_fit$dropped_covariate)
+        cov_new <- ncol(newer_fit$data$D$mu)
+        new_fit <- newer_fit
       }
-      if(re_n == 2){
-        ##Make better output
-        # AIC
-        new_fit$AIC <- c(AIC(fit), AIC(model))
-        names(new_fit$AIC) <- c("Input model", "Output model")
-        # Output_fit
+      # Make better output
+      if(new_fit$Note != "covariate dropped"){
+        newer_fit$dropped_covariate <- dropped
+        newer_fit$Note <- "covariate dropped"
+        newer_fit$Input_fit <- fit$call
+
         list <- list()
-        list$mu  <- mu
-        list$fit <- new_fit$Input_fit
-        new_fit$Note <- list
-        # Input_fit
-        new_fit$Input_fit <- fit$call
+        list$mu  <- newer_fit$formulae$mu
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
 
-        # Rename output list
-        names(new_fit) <- c("Input_fit", "Output_fit", "AIC")
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
+        names(newer_fit$AIC) <- c("Input model", "Output model")
+
       }
-
     }
-
-
+  }else{
+    newer_fit <- new_fit
   }
 
-  return(new_fit)
+  return(newer_fit)
 }
+
+
+# ----------------------------- sigma ---------------------------------
+
+#' @rdname backward_AIC
+#' @export
+backward_AIC_sigma <- function(fit){
+
+  new_fit <- drop1_AIC_sigma(fit)
+  # Store dropped_covariate for later use
+  dropped <- new_fit$dropped_covariate
+  #Check if the above new_fit is null model
+  if(new_fit$Note == "covariate dropped"){
+    cov_new <- ncol(new_fit$data$D$sigma)
+    # If we have dropped all the covariates then we stop
+    # Otherwise, we try to dropp more variables, one at a time, using
+    # drop1_AIC_sigma().  We stop when either
+    # 1. drop1_AIC_sigma() doesn't drop a covariate (new_fit$Note != "covariate dropped"), or
+    # 2. we have dropped all the covariates (cov_new == 1)
+    # Therefore, we continue to loop while new_fit$Note == "covariate dropped" and cov_new != 1
+    if(cov_new == 1){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (new_fit$Note == "covariate dropped" & cov_new != 1) {
+        newer_fit <- drop1_AIC_sigma(new_fit)
+        dropped <- append(dropped, newer_fit$dropped_covariate)
+        cov_new <- ncol(newer_fit$data$D$sigma)
+        new_fit <- newer_fit
+      }
+      # Make better output
+      if(new_fit$Note != "covariate dropped"){
+        newer_fit$dropped_covariate <- dropped
+        newer_fit$Note <- "covariate dropped"
+        newer_fit$Input_fit <- fit$call
+
+        list <- list()
+        list$sigma <- newer_fit$formulae$sigma
+        list$fit   <- newer_fit$call
+        newer_fit$Output_fit <- list
+
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
+        names(newer_fit$AIC) <- c("Input model", "Output model")
+
+      }
+    }
+  }else{
+    newer_fit <- new_fit
+  }
+
+  return(newer_fit)
+}
+
+
+
+# ----------------------------- xi ---------------------------------
+
+#' @rdname backward_AIC
+#' @export
+backward_AIC_xi <- function(fit){
+
+  new_fit <- drop1_AIC_xi(fit)
+  # Store dropped_covariate for later use
+  dropped <- new_fit$dropped_covariate
+  #Check if the above new_fit is null model
+  if(new_fit$Note == "covariate dropped"){
+    cov_new <- ncol(new_fit$data$D$xi)
+    # If we have dropped all the covariates then we stop
+    # Otherwise, we try to dropp more variables, one at a time, using
+    # drop1_AIC_xi().  We stop when either
+    # 1. drop1_AIC_xi() doesn't drop a covariate (new_fit$Note != "covariate dropped"), or
+    # 2. we have dropped all the covariates (cov_new == 1)
+    # Therefore, we continue to loop while new_fit$Note == "covariate dropped" and cov_new != 1
+    if(cov_new == 1){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (new_fit$Note == "covariate dropped" & cov_new != 1) {
+        newer_fit <- drop1_AIC_xi(new_fit)
+        dropped <- append(dropped, newer_fit$dropped_covariate)
+        cov_new <- ncol(newer_fit$data$D$xi)
+        new_fit <- newer_fit
+      }
+      # Make better output
+      if(new_fit$Note != "covariate dropped"){
+        newer_fit$dropped_covariate <- dropped
+        newer_fit$Note <- "covariate dropped"
+        newer_fit$Input_fit <- fit$call
+
+        list <- list()
+        list$xi  <- newer_fit$formulae$xi
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
+
+        newer_fit$AIC <- c(AIC(fit), AIC(newer_fit))
+        names(newer_fit$AIC) <- c("Input model", "Output model")
+
+      }
+    }
+  }else{
+    newer_fit <- new_fit
+  }
+
+  return(newer_fit)
+}
+
--- R/drop1_AIC.R
+++ R/drop1_AIC.R
@@ -1,38 +1,31 @@
-
 #' Drop one possible covariate on GEV parameter based on AIC
 #'
 #' Drop a single term to either mu, sigma, and xi based on AIC.
 #'
-#' drop1_AIC_mu(fit)
-#' drop1_AIC_sigma(fit)
-#' drop1_AIC_xi(fit)
-#'
-#' @param fit A model of class "gevreg".
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if a covariate has been added or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{dropped_covariate}{A character vector shows dropped covariate}
 #'     \item{AIC}{AIC values for both input model and output model if two models
 #'     are different.}
 #' @examples
 #'
 #' ### Annual Maximum and Minimum Temperature
-#' # Parameter mu
-#' library(extRemes)
-#' data(PORTw)
-#' PORTw$Year <- (PORTw$Year - min(PORTw$Year)) / (max(PORTw$Year) - min(PORTw$Year))
-#' P6 <- gevreg(TMX1, data = PORTw, mu = ~MTMAX + AOindex + Year + STDTMAX + STDMIN + MDTR)
+#'
+#' P6 <- gevreg(TMX1, data = PORTw[,-1], mu = ~MTMAX + AOindex + STDTMAX + STDMIN + MDTR)
+#' P7 <- gevreg(TMX1, data = PORTw[,-1], sigma = ~MTMAX + STDTMAX + STDMIN + MDTR)
+#' P8 <- gevreg(TMX1, data = PORTw[,-1], xi = ~MTMAX + STDTMAX + STDMIN + MDTR)
 #' drop1_AIC_mu(P6)
-#'
-#' # Parameter sigma
-#' P7 <- gevreg(TMX1, data = PORTw, mu = ~MTMAX + STDTMAX, sigma = ~MTMAX + STDTMAX)
 #' drop1_AIC_sigma(P7)
-#'
-#' # Parameter xi
+#' drop1_AIC_xi(P8)
 #'
 #'
 #' ### Oxford and Worthing annual maximum temperatures
@@ -42,8 +35,13 @@
 #' xi = ~loc, sigmalink = identity)
 #' drop1_AIC_mu(ow1)
 #'
-#'
-#'
+#' @name drop1_AIC
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname drop1_AIC
 #' @export
 drop1_AIC_mu <- function(fit){
   ##1. Check if input arguments are missing
@@ -56,7 +54,8 @@ drop1_AIC_mu <- function(fit){
   }
 
   ##3. Check if input fit has no covariates on mu
-  if(length(attr(fit$data$D$mu, "assign")) == 1){
+  n_mu <- ncol(fit$data$D$mu)
+  if(n_mu == 1){
     stop("Input fit has no covariate effects on mu")
   }
 
@@ -79,16 +78,32 @@ drop1_AIC_mu <- function(fit){
     X <- X[index]   #a data frame with covariates that are in the mu formula
     name <- names(X)
 
-
     #General steps
-    # 1. Fit all of the possible models obtained by dropping 1 in mu
-    #    covariate from the original model
+    # 1. Fit all of the possible models obtained by dropping 1 covariate from the original model
     # 2. Calculate the value of AIC for all these models.
     aic <- c()
     m_list <- list()
     for(i in 1:length(name)){
-      mu  <- update(fit$formulae$mu, paste("", name[i], sep = "~.-"))  #update mu formula
-      new_fit     <- update(fit, mu = mu)     #update fit
+      #dropping more variables on mu, one at a time
+      mu <- update(fit$formulae$mu, paste("", name[i], sep = "~.-")) #update mu formula
+      # update a model call by dropping one covariate on mu
+      # when we fit a new model in which an covariate is dropped,
+      # we use starting values based on the fit of the larger model.
+      fcoefs  <- fit$coefficients
+      which_to_drop <- paste0("mu: ", name[i])
+      drop_num <- which(which_to_drop == names(fcoefs))
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- fcoefs[1:n_mu][-drop_num]
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma)
+      new_fit     <- update(fit, mu = mu,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart = xistart)
       m_list[[i]] <- new_fit
       aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
     }
@@ -101,9 +116,10 @@ drop1_AIC_mu <- function(fit){
     #    return this model.  Otherwise, return the original model
 
     if(min(aic) < AIC(fit)){
-      output <- list()
+      output <- m_list[[x_i]]
+      output$dropped_covariate <- name[x_i]
+      output$Note <- "covariate dropped"
       output$Input_fit <- fit$call
-
       list <- list()
       list$mu  <- m_list[[x_i]]$formulae$mu
       list$fit <- m_list[[x_i]]$call
@@ -112,22 +128,24 @@ drop1_AIC_mu <- function(fit){
       output$AIC        <- c(AIC(fit), min(aic))
       names(output$AIC) <- c("Input model", "Output model")
 
-      return(output)
     }else{
-      output <- list()
+      output <- fit
+      output$dropped_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
 
   }
-  #else for pp fit
+  return(output)
 
 }
 
 
 
+# ----------------------------- sigma ---------------------------------
+
+#' @rdname drop1_AIC
+#' @export
 drop1_AIC_sigma <- function(fit){
   ##1. Check if input arguments are missing
   if(missing(fit))  stop("fit must be specified")
@@ -139,7 +157,8 @@ drop1_AIC_sigma <- function(fit){
   }
 
   ##3. Check if input fit has no covariates on sigma
-  if(length(attr(fit$data$D$sigma, "assign")) == 1){
+  n_sigma <- ncol(fit$data$D$sigma)
+  if(n_sigma == 1){
     stop("Input fit has no covariate effects on sigma")
   }
 
@@ -162,16 +181,32 @@ drop1_AIC_sigma <- function(fit){
     X <- X[index]   #a data frame with covariates that are in the sigma formula
     name <- names(X)
 
-
     #General steps
-    # 1. Fit all of the possible models obtained by dropping 1 covariate on sigma
-    #    from the original model
+    # 1. Fit all of the possible models obtained by dropping 1 covariate from the original model
     # 2. Calculate the value of AIC for all these models.
     aic <- c()
     m_list <- list()
     for(i in 1:length(name)){
-      sigma  <- update(fit$formulae$sigma, paste("", name[i], sep = "~.-"))  #update sigma formula
-      new_fit     <- update(fit, sigma = sigma)     #update fit
+      #dropping more variables on sigma, one at a time
+      sigma <- update(fit$formulae$sigma, paste("", name[i], sep = "~.-")) #update sigma formula
+      # update a model call by dropping one covariate on sigma
+      # when we fit a new model in which an covariate is dropped,
+      # we use starting values based on the fit of the larger model.
+      fcoefs  <- fit$coefficients
+      which_to_drop <- paste0("sigma: ", name[i])
+      drop_num <- which(which_to_drop == names(fcoefs))
+      n_mu <- ncol(fit$data$D$mu)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- fcoefs[(n_mu + 1):(n_mu + n_sigma)][-drop_num]
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma)
+      new_fit     <- update(fit, sigma = sigma,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart = xistart)
       m_list[[i]] <- new_fit
       aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
     }
@@ -184,27 +219,128 @@ drop1_AIC_sigma <- function(fit){
     #    return this model.  Otherwise, return the original model
 
     if(min(aic) < AIC(fit)){
-      output <- list()
+      output <- m_list[[x_i]]
+      output$dropped_covariate <- name[x_i]
+      output$Note <- "covariate dropped"
       output$Input_fit <- fit$call
-
       list <- list()
-      list$sigma  <- m_list[[x_i]]$formulae$sigma
+      list$sigma <- m_list[[x_i]]$formulae$sigma
+      list$fit   <- m_list[[x_i]]$call
+      output$Output_fit <- list
+
+      output$AIC        <- c(AIC(fit), min(aic))
+      names(output$AIC) <- c("Input model", "Output model")
+
+    }else{
+      output <- fit
+      output$dropped_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
+      output$Input_fit <- fit$call
+    }
+
+  }
+  return(output)
+
+}
+
+
+# ----------------------------- xi ---------------------------------
+
+#' @rdname drop1_AIC
+#' @export
+drop1_AIC_xi <- function(fit){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
+
+  ##2. Check if input fit is an 'evreg' objects
+  m <- deparse(substitute(fit))
+  if(!inherits(get(m, envir = parent.frame()), "evreg")){
+    stop("Use only with 'evreg' objects")
+  }
+
+  ##3. Check if input fit has no covariates on xi
+  n_xi <- ncol(fit$data$D$xi)
+  if(n_xi == 1){
+    stop("Input fit has no covariate effects on xi")
+  }
+
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
+
+  ##identify family from the fit
+
+  ###################################################################
+  ####----------------------------GEV----------------------------####
+  ###################################################################
+  if(inherits(get(m, envir = parent.frame()), "gev")){
+
+    ###Extract covariates on xi
+    x_name <- all.vars(fit$formulae$xi)
+    index  <- which(colnames(X) %in% x_name)
+
+    X <- X[index]   #a data frame with covariates that are in the xi formula
+    name <- names(X)
+
+    #General steps
+    # 1. Fit all of the possible models obtained by dropping 1 covariate from the original model
+    # 2. Calculate the value of AIC for all these models.
+    aic <- c()
+    m_list <- list()
+    for(i in 1:length(name)){
+      #dropping more variables on xi, one at a time
+      xi <- update(fit$formulae$xi, paste("", name[i], sep = "~.-")) #update xi formula
+      # update a model call by dropping one covariate on xi
+      # when we fit a new model in which an covariate is dropped,
+      # we use starting values based on the fit of the larger model.
+      fcoefs  <- fit$coefficients
+      n_mu <- ncol(fit$data$D$mu)
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma - 1)
+      new_fit     <- try(update(fit, xi = xi,
+                               mustart = mustart,
+                               sigmastart = sigmastart,
+                               xistart = xistart))
+      m_list[[i]] <- new_fit
+      aic[i]      <- AIC(m_list[[i]])         #get AIC for update fit
+    }
+
+    x_i  <- which(aic == min(aic))
+
+
+    # 3. Identify which of these models has the smallest AIC.
+    # 4. If this AIC is smaller than that of the current model then
+    #    return this model.  Otherwise, return the original model
+
+    if(min(aic) < AIC(fit)){
+      output <- m_list[[x_i]]
+      output$dropped_covariate <- name[x_i]
+      output$Note <- "covariate dropped"
+      output$Input_fit <- fit$call
+      list <- list()
+      list$xi  <- m_list[[x_i]]$formulae$xi
       list$fit <- m_list[[x_i]]$call
       output$Output_fit <- list
 
       output$AIC        <- c(AIC(fit), min(aic))
       names(output$AIC) <- c("Input model", "Output model")
 
-      return(output)
     }else{
-      output <- list()
+      output <- fit
+      output$dropped_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
 
   }
-  #else for pp fit
+  return(output)
 
 }
+
