da0b4f05d0c0dc9ce258a4dcd83fe66acd5b7e65
pengyuwei94 <pengyuwei1994@gmail.com>
2019-08-08 21:09:47 +0100

    complete version with no bugs

--- R/add1_pvalue.R
+++ R/add1_pvalue.R
@@ -1,21 +1,20 @@
-#' Add one possible covariate on GEV parameter using individual p value
+#' Add one possible covariate on GEV parameter using individual p value from Wald test
 #'
-#' Add a single term to either mu, sigma, and xi based on individual p value.
+#' Add a single term to either mu, sigma, and xi based on individual p value from Wald test.
 #'
-#' add1_p_mu(fit, alpha = 0.05)
-#' add1_p_sigma(fit, alpha = 0.05)
-#' add1_p_xi(fit, alpha = 0.05)
-#'
-#' @param fit A model of class "gevreg".
-#' @param alpha Significance level. Default value is 0.05..
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
+#' @param alpha Significance level. Default value is 0.05.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if a covariate has been added or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{added_covariate}{A character vector shows added covariate}
 #'     \item{pvalue}{A data frame that contains p value with five decimal
 #'     places of the added covariate if there is one.}
 #' @examples
@@ -25,6 +24,19 @@
 #' f0 <- gevreg(SeaLevel, data = evreg::fremantle)
 #' add1_p_mu(f0)
 #'
+#'
+#' ### Annual Maximum and Minimum Temperature
+#'
+#' P0 <- gevreg(y = TMX1, data = PORTw[, -1])
+#' add1_p_mu(P0)
+#'
+#' @name add1_p
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname add1_p
 #' @export
 add1_p_mu <- function(fit, alpha = 0.05){
   ##1. Check if input arguments are missing
@@ -43,7 +55,7 @@ add1_p_mu <- function(fit, alpha = 0.05){
   n       <- ncol(X) + 1          #number of covatiates +1 in the data
 
   ##3. Check if input fit already has all covariates on mu
-  n_mu <- length(attr(fit$data$D$mu, "assign"))
+  n_mu <- ncol(fit$data$D$mu)
   if(n_mu == n){
     stop("All covariates has been added on mu for input fit")
   }
@@ -62,89 +74,285 @@ add1_p_mu <- function(fit, alpha = 0.05){
 
     ###Extract covariates on mu if there are in input fit
     if(n_mu != 1){
-
       x_name <- all.vars(fit$formulae$mu)
       index  <- which(colnames(X) %in% x_name)
-
-      X <- X[-index]   #a data frame with covariates that are not in the sigma formula
-
+      X <- X[-index]   #a data frame with covariates that are not in the mu formula
     }
     name <- names(X)   #get names from rest of covariates
-
     #General steps
     # 1. Fit all of the possible models obtained by adding 1 covariate to the original model
     # 2. Obtain the individual p value of the added covariate.
 
-    ##Likelihood-ratio-test
     p_vec   <- c()
     m_list  <- list()
-
-    # Preparation for updating initial values.
-    # Detail explanations are in the for loop.
-    #n_sigma <- length(attr(fit$data$D$sigma, "assign"))
-    #n_coeff <- length(fit$coefficients)
-
-    #muint   <- c(unname(fit$coefficients[1:n_mu]), 0)
-    #sigint  <- unname(fit$coefficients[(n_mu + 1):(n_mu + n_sigma)])
-    #xiint   <- unname(fit$coefficients[(n_mu + n_sigma + 1):n_coeff])
-
-    #muint   <- deparse(substitute(muint))
-    #sigint  <- deparse(substitute(sigint))
-    #xiint   <- deparse(substitute(xiint))
-
-
-
     for(i in 1:length(name)){
       # adding more variables on mu, one at a time
-      mu          <- update(fit$formulae$mu, paste("", name[i], sep = "~.+"))  #update mu formula
-
+      mu <- update(fit$formulae$mu, paste("", name[i], sep = "~.+"))  #update mu formula
       # update a model call by adding one additional covariate on mu
       # when we fit a new model in which an extra covariate is added,
       # we use starting values based on the fit of the smaller model.
       # The start value for the new added variable will be set to be zero.
-      m_list[[i]] <- update(fit, mu = mu
-                            #,
-                            #mustart = muint,
-                            #sigmastart = sigint,
-                            #xistart = xiint
-      )
+      fcoefs <- fit$coefficients
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- c(unname(fcoefs[1:n_mu]), 0)
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      xistart <- unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)])
+      m_list[[i]] <- update(fit, mu = mu,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart = xistart)
 
 
       fit2        <- m_list[[i]]
       p_vec[i]  <- unname(summary(fit2)$pvalue)[n_mu+1] #store all the p-values in one vector
     }
+
+    # Check if there is NA values in the p_vec, if so, set it to Infinite
+    if(any(is.na(p_vec)) == TRUE){
+      na_index <- which(is.na(p_vec) == TRUE)
+      p_vec[na_index] <- Inf
+    }
     x_i  <- which(p_vec == min(p_vec))
 
     ##Check significance
-    # 3. If none of the p values are significant, return a list that contains three things.
-    # Otherwise, return a list of length two.
-    if(min(p_vec) < alpha){
-      output <- list()
+    # 3. If none of the p values are significant, return a new fitted object.
+    #    Otherwise, return the old fitted object.
+    if (min(p_vec) < alpha) {
+      # Base the output mainly on the chosen fitted model object
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
       output$Input_fit <- fit$call
-
       list <- list()
       list$mu  <- m_list[[x_i]]$formulae$mu
       list$fit <- m_list[[x_i]]$call
       output$Output_fit <- list
-
       output$pvalue <- as.data.frame(round(p_vec[x_i],5))
       row.names(output$pvalue) <- name[x_i]
-      colnames(output$pvalue)  <- c("Pr(>|z|)")
-
-      return(output)
-    }else{
-      output <- list()
+      colnames(output$pvalue)  <- c("pvalue from Wald test")
+    } else {
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note <- "Input fit and output fit are the same"
       output$Input_fit <- fit$call
-      output$Note      <- ("Input fit and output fit are the same.")
-
-      return(output)
     }
   }
-  #else for pp fit
-
+  return(output)
 }
 
 
+# ----------------------------- sigma ---------------------------------
+
+#' @rdname add1_p
+#' @export
+add1_p_sigma <- function(fit, alpha = 0.05){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
+
+  ##2. Check if input fit is an 'evreg' objects
+  m <- deparse(substitute(fit))
+  if(!inherits(get(m, envir = parent.frame()), "evreg")){
+    stop("Use only with 'evreg' objects")
+  }
+
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
+  n       <- ncol(X) + 1          #number of covatiates +1 in the data
+
+  ##3. Check if input fit already has all covariates on sigma
+  n_sigma <- ncol(fit$data$D$sigma)
+  if(n_sigma == n){
+    stop("All covariates has been added on sigma for input fit")
+  }
+
+  ##4. Check if input alpha is valid
+  if(alpha>1 || alpha <0){
+    stop("Significance level alpha should be smaller than 1 and larger than 0")
+  }
+
+  ##identify family from the fit
+
+  ###################################################################
+  ####----------------------------GEV----------------------------####
+  ###################################################################
+  if(inherits(get(m, envir = parent.frame()), "gev")){
+
+    ###Extract covariates on sigma if there are in input fit
+    if(n_sigma != 1){
+      x_name <- all.vars(fit$formulae$sigma)
+      index  <- which(colnames(X) %in% x_name)
+      X <- X[-index]   #a data frame with covariates that are not in the sigma formula
+    }
+    name <- names(X)   #get names from rest of covariates
+    #General steps
+    # 1. Fit all of the possible models obtained by adding 1 covariate to the original model
+    # 2. Obtain the individual p value of the added covariate.
+
+    p_vec   <- c()
+    m_list  <- list()
+    for(i in 1:length(name)){
+      # adding more variables on sigma, one at a time
+      sigma <- update(fit$formulae$sigma, paste("", name[i], sep = "~.+"))  #update sigma formula
+      # update a model call by adding one additional covariate on sigma
+      # when we fit a new model in which an extra covariate is added,
+      # we use starting values based on the fit of the smaller model.
+      # The start value for the new added variable will be set to be zero.
+      fcoefs <- fit$coefficients
+      n_mu   <- ncol(fit$data$D$mu)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- c(unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)]), 0)
+      xistart <- unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)])
+      m_list[[i]] <- update(fit, sigma = sigma,
+                            mustart = mustart,
+                            sigmastart = sigmastart,
+                            xistart = xistart)
+
+
+      fit2        <- m_list[[i]]
+      p_vec[i]  <- unname(summary(fit2)$pvalue)[n_mu + n_sigma + 1] #store all the p-values in one vector
+    }
+
+    # Check if there is NA values in the p_vec, if so, set it to Infinite
+    if(any(is.na(p_vec)) == TRUE){
+      na_index <- which(is.na(p_vec) == TRUE)
+      p_vec[na_index] <- Inf
+    }
+    x_i  <- which(p_vec == min(p_vec))
+
+    ##Check significance
+    # 3. If none of the p values are significant, return a new fitted object.
+    #    Otherwise, return the old fitted object.
+    if (min(p_vec) < alpha) {
+      # Base the output mainly on the chosen fitted model object
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
+      output$Input_fit <- fit$call
+      list <- list()
+      list$sigma <- m_list[[x_i]]$formulae$sigma
+      list$fit   <- m_list[[x_i]]$call
+      output$Output_fit <- list
+      output$pvalue <- as.data.frame(round(p_vec[x_i],5))
+      row.names(output$pvalue) <- name[x_i]
+      colnames(output$pvalue)  <- c("pvalue from Wald test")
+    } else {
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note <- "Input fit and output fit are the same"
+      output$Input_fit <- fit$call
+    }
+  }
+  return(output)
+}
+
+
+# ----------------------------- xi ---------------------------------
+
+#' @rdname add1_p
+#' @export
+add1_p_xi <- function(fit, alpha = 0.05){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
+
+  ##2. Check if input fit is an 'evreg' objects
+  m <- deparse(substitute(fit))
+  if(!inherits(get(m, envir = parent.frame()), "evreg")){
+    stop("Use only with 'evreg' objects")
+  }
+
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
+  n       <- ncol(X) + 1          #number of covatiates +1 in the data
+
+  ##3. Check if input fit already has all covariates on xi
+  n_xi <- ncol(fit$data$D$xi)
+  if(n_xi == n){
+    stop("All covariates has been added on xi for input fit")
+  }
+
+  ##4. Check if input alpha is valid
+  if(alpha>1 || alpha <0){
+    stop("Significance level alpha should be smaller than 1 and larger than 0")
+  }
+
+  ##identify family from the fit
+
+  ###################################################################
+  ####----------------------------GEV----------------------------####
+  ###################################################################
+  if(inherits(get(m, envir = parent.frame()), "gev")){
+
+    ###Extract covariates on xi if there are in input fit
+    if(n_xi != 1){
+      x_name <- all.vars(fit$formulae$xi)
+      index  <- which(colnames(X) %in% x_name)
+      X <- X[-index]   #a data frame with covariates that are not in the xi formula
+    }
+    name <- names(X)   #get names from rest of covariates
+    #General steps
+    # 1. Fit all of the possible models obtained by adding 1 covariate to the original model
+    # 2. Obtain the individual p value of the added covariate.
+
+    p_vec   <- c()
+    m_list  <- list()
+    for(i in 1:length(name)){
+      # adding more variables on xi, one at a time
+      xi <- update(fit$formulae$xi, paste("", name[i], sep = "~.+"))  #update xi formula
+      # update a model call by adding one additional covariate on xi
+      # when we fit a new model in which an extra covariate is added,
+      # we use starting values based on the fit of the smaller model.
+      # The start value for the new added variable will be set to be zero.
+      fcoefs  <- fit$coefficients
+      n_mu    <- ncol(fit$data$D$mu)
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      xistart <- c(unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)]), 0)
+      m_list[[i]] <- try(update(fit, xi = xi,
+                               mustart = mustart,
+                               sigmastart = sigmastart,
+                               xistart = xistart))
+
+
+      fit2        <- m_list[[i]]
+      p_vec[i]  <- unname(summary(fit2)$pvalue)[length(fcoefs)+1] #store all the p-values in one vector
+    }
+
+    # Check if there is NA values in the p_vec, if so, set it to Infinite
+    if(any(is.na(p_vec)) == TRUE){
+      na_index <- which(is.na(p_vec) == TRUE)
+      p_vec[na_index] <- Inf
+    }
+    x_i  <- which(p_vec == min(p_vec))
+    ##Check significance
+    # 3. If none of the p values are significant, return a new fitted object.
+    #    Otherwise, return the old fitted object.
+    if (min(p_vec) < alpha) {
+      # Base the output mainly on the chosen fitted model object
+      output <- m_list[[x_i]]
+      output$added_covariate <- name[x_i]
+      output$Note <- "covariate added"
+      output$Input_fit <- fit$call
+      list <- list()
+      list$xi  <- m_list[[x_i]]$formulae$xi
+      list$fit <- m_list[[x_i]]$call
+      output$Output_fit <- list
+      output$pvalue <- as.data.frame(round(p_vec[x_i],5))
+      row.names(output$pvalue) <- name[x_i]
+      colnames(output$pvalue)  <- c("pvalue from Wald test")
+    } else {
+      output <- fit
+      output$added_covariate <- NULL
+      output$Note <- "Input fit and output fit are the same"
+      output$Input_fit <- fit$call
+    }
+  }
+  return(output)
+}
 
 
 
--- R/forward_pvalue.R
+++ R/forward_pvalue.R
@@ -1,24 +1,24 @@
-#' Forward Selection on GEV Parameter using individual p value
+#' Forward Selection on GEV Parameter using individual p value from Wald test
 #'
 #' Significance controlled variable selection selects variables in either
-#' mu, sigma, and xi with forward direction based on individual p value.
+#' mu, sigma, and xi with forward direction based on individual p value from Wald test.
 #'
-#' forward_p_mu(fit, alpha = 0.05)
-#' forward_p_sigma(fit, alpha = 0.05)
-#' forward_p_xi(fit, alpha = 0.05)
 #'
-#' @param fit A model of class "gevreg".
-#' @param alpha Significance level. Default value is 0.05..
+#' @param fit An object of class \code{c("gev", "evreg")} returned from
+#'   \code{\link{gevreg}} summarising the current model fit.
+#' @param alpha Significance level. Default value is 0.05.
 #' @details Add details.
-#' @return A list which has the following components
-#'     \item{Input_fit}{The input object of the class gevreg.}
-#'     \item{Note}{A message that will be printed when input fit and output
-#'     fit are the same.}
+#' @return An object (a list) of class \code{c("gev", "evreg")} summarising
+#'   the new model fit (which may be the same as \code{fit}) and containing the
+#'   following additional components
+#'     \item{Input_fit}{The input object of the class \code{c("gev", "evreg")}.}
+#'     \item{Note}{A message that tells if a covariate has been added or not.}
 #'     \item{Output_fit}{A list that contains formulae for the parameter,
-#'     and the output object of the class gevreg if output fit is different
-#'     from the input fit.}
+#'     and the output object of the class \code{c("gev", "evreg")} if the output fit
+#'     is different from the input fit.}
+#'     \item{added_covariate}{A character vector shows added covariates}
 #'     \item{pvalue}{A data frame that contains p values with five decimal
-#'     places of the added covariates in order if there are.}
+#'     places of the added covariates if there are some.}
 #' @examples
 #'
 #' ### Fremantle sea levels
@@ -26,64 +26,183 @@
 #' f0 <- gevreg(SeaLevel, data = evreg::fremantle)
 #' forward_p_mu(f0)
 #'
+#'
+#' ### Annual Maximum and Minimum Temperature
+#'
+#' P0 <- gevreg(y = TMX1, data = PORTw[, -1])
+#' forward_p_mu(P0)
+#' forward_p_sigma(P0)
+#' forward_p_xi(P0)
+#' @name forward_p
+NULL
+## NULL
+
+# ----------------------------- mu ---------------------------------
+
+#' @rdname forward_p
 #' @export
 forward_p_mu <- function(fit, alpha = 0.05){
-
   # Number of covariates in the data
   cov_n   <- ncol(eval(fit$call$data)) - 1
   new_fit <- add1_p_mu(fit, alpha)
-
   # Store p table for later use
   p_table <- new_fit$pvalue
-
-
-  # The length of return list from new_fit
-  # If this is equal to 3 then we have added a covariate
-  # If this is equal to 2 then we haven't
-  re_n    <- as.numeric(length(new_fit))
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
 
   #Check if the above new_fit is full model
-  if(re_n != 2){
+  if(new_fit$Note == "covariate added"){
     cov_new <- length(all.vars(new_fit$Output_fit$mu))
     # If we have added all the covariates then we stop
     # Otherwise, we try adding more variables, one at a time, using
     # add1_p_mu().  We stop when either
-    # 1. add1_p_mu() doesn't add a covariate (re_n = 2), or
+    # 1. add1_p_mu() doesn't add a covariate (new_fit$Note != "covariate added"), or
     # 2. we have added all the covariates (cov_new = cov_n)
-    # Therefore, we continue to loop while re_n != 2 and cov_new < cov_n
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
     if(cov_new == cov_n){
-      return(new_fit)
+      newer_fit <- new_fit
     }else{
-      while (re_n != 2 & cov_new < cov_n) {
-        mu      <- new_fit$Output_fit$mu
-        fit     <- eval(new_fit$Output_fit$fit)
-        new_fit <- add1_p_mu(fit, alpha)
-        # Improve the p value data frame from add1_p_mu()
-        new_p   <- new_fit$pvalue
-        p_table <- rbind(p_table, new_p)
-
-        re_n    <- as.numeric(length(new_fit))   #length of return list from new_fit
-        cov_new <- length(all.vars(new_fit$Output_fit$mu))
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_p_mu(new_fit, alpha)
+        p_table   <- rbind(p_table, newer_fit$pvalue)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(newer_fit$formulae$mu))
+        new_fit   <- newer_fit
       }
-      if(re_n == 2){
-        ##Make better output
-        # Output_fit
+      # Make a better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+
         list <- list()
-        list$mu  <- mu
-        list$fit <- new_fit$Input_fit
-        new_fit$Note <- list
-        # Input_fit
-        new_fit$Input_fit <- fit$call
-        # Update p table
-        new_fit$pvalue <- p_table
+        list$mu  <- newer_fit$formulae$mu
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
 
-        # Rename output list
-        names(new_fit) <- c("Input_fit", "Output_fit", "pvalue")
+        newer_fit$pvalue <- head(p_table,-1)
+
       }
-    }
-  }
 
-  return(new_fit)
+    }
+  } else {
+    newer_fit <- new_fit
+  }
+  return(newer_fit)
+}
+
+
+# ----------------------------- sigma ---------------------------------
+
+#' @rdname forward_p
+#' @export
+forward_p_sigma <- function(fit, alpha = 0.05){
+  # Number of covariates in the data
+  cov_n   <- ncol(eval(fit$call$data)) - 1
+  new_fit <- add1_p_sigma(fit, alpha)
+  # Store p table for later use
+  p_table <- new_fit$pvalue
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
+
+  #Check if the above new_fit is full model
+  if(new_fit$Note == "covariate added"){
+    cov_new <- length(all.vars(new_fit$Output_fit$sigma))
+    # If we have added all the covariates then we stop
+    # Otherwise, we try adding more variables, one at a time, using
+    # add1_p_sigma().  We stop when either
+    # 1. add1_p_sigma() doesn't add a covariate (new_fit$Note != "covariate added"), or
+    # 2. we have added all the covariates (cov_new = cov_n)
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
+    if(cov_new == cov_n){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_p_sigma(new_fit, alpha)
+        p_table   <- rbind(p_table, newer_fit$pvalue)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(newer_fit$formulae$sigma))
+        new_fit   <- newer_fit
+      }
+      # Make a better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+
+        list <- list()
+        list$sigma <- newer_fit$formulae$sigma
+        list$fit   <- newer_fit$call
+        newer_fit$Output_fit <- list
+
+        newer_fit$pvalue <- head(p_table,-1)
+
+      }
+
+    }
+  } else {
+    newer_fit <- new_fit
+  }
+  return(newer_fit)
+}
+
+
+
+# ----------------------------- xi ---------------------------------
+
+#' @rdname forward_p
+#' @export
+forward_p_xi <- function(fit, alpha = 0.05){
+  # Number of covariates in the data
+  cov_n   <- ncol(eval(fit$call$data)) - 1
+  new_fit <- add1_p_xi(fit, alpha)
+  # Store p table for later use
+  p_table <- new_fit$pvalue
+  # Store added_covariate for later use
+  added   <- new_fit$added_covariate
+
+  #Check if the above new_fit is full model
+  if(new_fit$Note == "covariate added"){
+    cov_new <- length(all.vars(new_fit$Output_fit$xi))
+    # If we have added all the covariates then we stop
+    # Otherwise, we try adding more variables, one at a time, using
+    # add1_p_xi().  We stop when either
+    # 1. add1_p_xi() doesn't add a covariate (new_fit$Note != "covariate added"), or
+    # 2. we have added all the covariates (cov_new = cov_n)
+    # Therefore, we continue to loop while new_fit$Note == "covariate added" and cov_new < cov_n
+    if(cov_new == cov_n){
+      newer_fit <- new_fit
+    }else{
+      newer_fit <- new_fit
+      while (newer_fit$Note == "covariate added" & cov_new < cov_n) {
+        newer_fit <- add1_p_xi(new_fit, alpha)
+        p_table   <- rbind(p_table, newer_fit$pvalue)
+        added     <- append(added, newer_fit$added_covariate)
+        cov_new   <- length(all.vars(newer_fit$formulae$xi))
+        new_fit   <- newer_fit
+      }
+      # Make a better output
+      if(newer_fit$Note != "covariate added"){
+        newer_fit$added_covariate <- added
+        newer_fit$Note <- "covariate added"
+        newer_fit$Input_fit <- fit$call
+
+        list <- list()
+        list$xi  <- newer_fit$formulae$xi
+        list$fit <- newer_fit$call
+        newer_fit$Output_fit <- list
+
+        newer_fit$pvalue <- head(p_table,-1)
+
+      }
+
+    }
+  } else {
+    newer_fit <- new_fit
+  }
+  return(newer_fit)
 }
 
 
