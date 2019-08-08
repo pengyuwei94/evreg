d591c91418395c96d942db35ab0e7fee40f00e74
pengyuwei94 <pengyuwei1994@gmail.com>
2019-08-08 16:35:40 +0100

    complete version with no bugs

--- R/drop1_LRT.R
+++ R/drop1_LRT.R
@@ -25,6 +25,16 @@
 #' ow1 <- gevreg(y = temp, data = ow[-3], mu = ~loc + year, sigma = ~loc,
 #' xi = ~loc, sigmalink = identity)
 #' drop1_LRT_mu(ow1)
+#'
+#'
+#' ### Annual Maximum and Minimum Temperature
+#'
+#' P3 <- gevreg(y = TMX1, data = PORTw[, -1], mu = ~MTMAX + STDTMAX + STDMIN)
+#' P4 <- gevreg(y = TMX1, data = PORTw[, -1], sigma = ~MTMAX + STDTMAX + STDMIN)
+#' P5 <- gevreg(y = TMX1, data = PORTw[, -1], xi = ~MTMAX + AOindex)
+#' drop1_LRT_mu(P3)
+#' drop1_LRT_sigma(P4)
+#' drop1_LRT_xi(P5)
 #' @name drop1_LRT
 NULL
 ## NULL
@@ -82,20 +92,25 @@ drop1_LRT_mu <- function(fit, alpha = 0.05){
     m_list  <- list()
     for(i in 1:length(name)){
       #dropping more variables on mu, one at a time
-      mu          <- update(fit$formulae$mu, paste("", name[i], sep = "~.-")) #update mu formula
+      mu <- update(fit$formulae$mu, paste("", name[i], sep = "~.-")) #update mu formula
       # update a model call by dropping one covariate on mu
       # when we fit a new model in which an covariate is dropped,
       # we use starting values based on the fit of the larger model.
       fcoefs  <- fit$coefficients
+      which_to_drop <- paste0("mu: ", name[i])
+      drop_num <- which(which_to_drop == names(fcoefs))
       n_sigma <- ncol(fit$data$D$sigma)
-      mustart <- c((unname(fcoefs[1:i])), unname(fcoefs[((i+2):n_mu)]))
+      mustart <- fcoefs[1:n_mu][-drop_num]
       sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
-      xistart <- unname(fcoefs[(n_mu + n_sigma + 1):length(fcoefs)])
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma)
       m_list[[i]] <- update(fit, mu = mu,
                             mustart = mustart,
                             sigmastart = sigmastart,
                             xistart = xistart)
-
 
       fit2        <- m_list[[i]]
       p_vec[i]    <- compare_pvalue(fit2, fit)      #store all the p-values in one vector
@@ -131,7 +146,209 @@ drop1_LRT_mu <- function(fit, alpha = 0.05){
 
 # ----------------------------- sigma ---------------------------------
 
+#' @rdname drop1_LRT
+#' @export
+drop1_LRT_sigma <- function(fit, alpha = 0.05){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
+
+  ##2. Check if input fit is an 'evreg' objects
+  m <- deparse(substitute(fit))
+  if(!inherits(get(m, envir = parent.frame()), "evreg")){
+    stop("Use only with 'evreg' objects")
+  }
+
+  ##3. Check if there are covariate effects on sigma
+  n_sigma <- ncol(fit$data$D$sigma)
+  if(n_sigma == 1){
+    stop("Input fit has no covariate effects on sigma")
+  }
+
+  ##4. Check if input alpha is valid
+  if(alpha>1 || alpha <0){
+    stop("Significance level alpha should be smaller than 1 and larger than 0")
+  }
+
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
+
+  ##identify family from the fit
+
+  ###################################################################
+  ####----------------------------GEV----------------------------####
+  ###################################################################
+  if(inherits(get(m, envir = parent.frame()), "gev")){
+
+    ###Extract existing covariates on sigma
+    x_name <- all.vars(fit$formulae$sigma)
+    index  <- which(colnames(X) %in% x_name)
+
+    X <- X[index]   #a data frame with covariates that are in the sigma formula
+    name <- names(X)
+
+    #General steps
+    # 1. Fit all of the possible models obtained by dropping 1 covariate from the original model
+    # 2. Calculate the p value between new model and original model.
+
+    ##likelihood-ratio-test
+    p_vec   <- c()
+    m_list  <- list()
+    for(i in 1:length(name)){
+      #dropping more variables on sigma, one at a time
+      sigma <- update(fit$formulae$sigma, paste("", name[i], sep = "~.-")) #update sigma formula
+      # update a model call by dropping one covariate on sigma
+      # when we fit a new model in which an covariate is dropped,
+      # we use starting values based on the fit of the larger model.
+      fcoefs  <- fit$coefficients
+      which_to_drop <- paste0("sigma: ", name[i])
+      drop_num <- which(which_to_drop == names(fcoefs))
+      n_mu <- ncol(fit$data$D$mu)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- fcoefs[(n_mu + 1):(n_mu + n_sigma)][-drop_num]
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma)
+      m_list[[i]] <- (update(fit, sigma = sigma,
+                             mustart = mustart,
+                             sigmastart = sigmastart,
+                             xistart = xistart))
+
+      fit2        <- m_list[[i]]
+      p_vec[i]    <- compare_pvalue(fit2, fit)      #store all the p-values in one vector
+    }
+    x_i  <- which(p_vec == max(p_vec))
+
+    ##check significance
+    # 3. If none of the p values are significant, return a new fitted object.
+    #    Otherwise, return the old fitted object.
+    if(max(p_vec) > alpha){
+      output <- m_list[[x_i]]
+      output$dropped_covariate <- name[x_i]
+      output$Note <- "covariate dropped"
+      output$Input_fit <- fit$call
+      list <- list()
+      list$sigma <- m_list[[x_i]]$formulae$sigma
+      list$fit   <- m_list[[x_i]]$call
+      output$Output_fit <- list
+      output$pvalue <- as.data.frame(round(p_vec[x_i],5))
+      row.names(output$pvalue) <- name[x_i]
+      colnames(output$pvalue)  <- c("LRT_pvalue")
+
+    }else{
+      output <- fit
+      output$dropped_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
+      output$Input_fit <- fit$call
+
+    }
+  }
+  return(output)
+}
 
 # ----------------------------- xi ---------------------------------
 
+#' @rdname drop1_LRT
+#' @export
+drop1_LRT_xi <- function(fit, alpha = 0.05){
+  ##1. Check if input arguments are missing
+  if(missing(fit))  stop("fit must be specified")
 
+  ##2. Check if input fit is an 'evreg' objects
+  m <- deparse(substitute(fit))
+  if(!inherits(get(m, envir = parent.frame()), "evreg")){
+    stop("Use only with 'evreg' objects")
+  }
+
+  ##3. Check if there are covariate effects on xi
+  n_xi <- ncol(fit$data$D$xi)
+  if(n_xi == 1){
+    stop("Input fit has no covariate effects on xi")
+  }
+
+  ##4. Check if input alpha is valid
+  if(alpha>1 || alpha <0){
+    stop("Significance level alpha should be smaller than 1 and larger than 0")
+  }
+
+  data    <- eval(fit$call$data)         #save data
+  y       <- fit$call$y                  #response variable
+  y_index <- which(colnames(data) == y)  #index of column y
+  X       <- data[-y_index]              #data only with covariates
+
+  ##identify family from the fit
+
+  ###################################################################
+  ####----------------------------GEV----------------------------####
+  ###################################################################
+  if(inherits(get(m, envir = parent.frame()), "gev")){
+
+    ###Extract existing covariates on xi
+    x_name <- all.vars(fit$formulae$xi)
+    index  <- which(colnames(X) %in% x_name)
+
+    X <- X[index]   #a data frame with covariates that are in the mu formula
+    name <- names(X)
+
+    #General steps
+    # 1. Fit all of the possible models obtained by dropping 1 covariate from the original model
+    # 2. Calculate the p value between new model and original model.
+
+    ##likelihood-ratio-test
+    p_vec   <- c()
+    m_list  <- list()
+    for(i in 1:length(name)){
+      #dropping more variables on xi, one at a time
+      xi <- update(fit$formulae$xi, paste("", name[i], sep = "~.-")) #update xi formula
+      # update a model call by dropping one covariate on xi
+      # when we fit a new model in which an covariate is dropped,
+      # we use starting values based on the fit of the larger model.
+      fcoefs  <- fit$coefficients
+      n_mu    <- ncol(fit$data$D$mu)
+      n_sigma <- ncol(fit$data$D$sigma)
+      mustart <- unname(fcoefs[1:n_mu])
+      sigmastart <- unname(fcoefs[(n_mu + 1):(n_mu + n_sigma)])
+      # Non-zero components of xistart may mean that the likelihood is zero
+      # at the starting values.  This is because for xi not equal to zero
+      # there is a constraint on the parameter space.  To avoid this set all
+      # components of xistart to 0, i.e. the Gumbel case.
+      xistart <- rep(0, length(fcoefs) - n_mu - n_sigma - 1)
+      m_list[[i]] <- try(update(fit, xi = xi,
+                               mustart = mustart,
+                               sigmastart = sigmastart,
+                               xistart = xistart))
+
+      fit2        <- m_list[[i]]
+      p_vec[i]    <- compare_pvalue(fit2, fit)      #store all the p-values in one vector
+    }
+    x_i  <- which(p_vec == max(p_vec))
+
+    ##check significance
+    # 3. If none of the p values are significant, return a new fitted object.
+    #    Otherwise, return the old fitted object.
+    if(max(p_vec) > alpha){
+      output <- m_list[[x_i]]
+      output$dropped_covariate <- name[x_i]
+      output$Note <- "covariate dropped"
+      output$Input_fit <- fit$call
+      list <- list()
+      list$xi  <- m_list[[x_i]]$formulae$xi
+      list$fit <- m_list[[x_i]]$call
+      output$Output_fit <- list
+      output$pvalue <- as.data.frame(round(p_vec[x_i],5))
+      row.names(output$pvalue) <- name[x_i]
+      colnames(output$pvalue)  <- c("LRT_pvalue")
+
+    }else{
+      output <- fit
+      output$dropped_covariate <- NULL
+      output$Note      <- "Input fit and output fit are the same"
+      output$Input_fit <- fit$call
+
+    }
+  }
+  return(output)
+}
